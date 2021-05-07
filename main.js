a3d403eaf8242b84e41fd0767c435e12d778f057
Anurag Tiwari <anurag1504t@gmail.com>
2021-05-07 17:58:30 +0530

    V2

--- main.js
+++ main.js
@@ -15,19 +15,19 @@ for(let i=0;i<list.length;i++) {
 }
 
 let N = nodes.size;
-let graph = Array(N).fill(0).map(x => Array(N).fill(0))
+let amount = Array(N).fill(0);
 
 for(let i=0;i<list.length;i++) {
     let paidBy = list[i].paidBy;
     const keys = Object.keys(list[i].paidFor);
     keys.forEach((key, index) => {
-        graph[paidBy.charCodeAt(0)-65][key.charCodeAt(0)-65] += list[i].paidFor[key];
+        amount[paidBy.charCodeAt(0)-65] -= list[i].paidFor[key];
+        amount[key.charCodeAt(0)-65] += list[i].paidFor[key];
     });
 }
+console.log(amount);
 
-console.log(graph);
-
-function minimize(amount)	{
+function minimize()	{
     var maxCredit = amount.indexOf(Math.max(...amount)), maxDebit = amount.indexOf(Math.min(...amount));
 	if (amount[maxCredit] == 0 && amount[maxDebit] == 0)
 		return;
@@ -35,22 +35,12 @@ function minimize(amount)	{
 	amount[maxCredit] -= min;
 	amount[maxDebit] += min;
     console.log(`${String.fromCharCode(65 + maxCredit)} owes ${String.fromCharCode(65 + maxDebit)} ${min}`);
-	minimize(amount);
+	minimize();
 }
 
-function minTranc(graph) {
-	var amount = new Array(N).fill(0);;
-	for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            amount[i] += (graph[j][i] - graph[i][j]);
-        }            
-    }   
-    if(amount.reduce( (x,y) => {return x+y}) != 0) {
-        console.log("Transaction Unbalanced");
-    }   
-    else {
-        minimize(amount);
-    }	
-}
-
-minTranc(graph);
+if(amount.reduce( (x,y) => {return x+y}) != 0) {
+    console.log("Transactions Unbalanced/Inconsistent");
+}   
+else {
+    minimize();
+}	
